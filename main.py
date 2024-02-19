c8bb60e79b60430982d98d20d349f1f6fe0d8ccc
dvillaal <dvillaal@unal.edu.co>
2024-02-19 18:31:53 -0500

    prueba 1

---
+++ main.py
@@ -0,0 +1,51 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self. precio = precio
+        self.registro = registro
+
+    def CambiarColor(self, color):
+        if (color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco" ):
+            self.color = color
+
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.cantidadCreados = cantidadCreados
+    
+    def cantidadAsientos(self):
+        totalAsientos = 0
+        for asiento in self.asientos:
+            if (type(asiento) == Asiento):
+                totalAsientos += 1
+        return totalAsientos
+    
+    def verificarIntegridad(self):
+        if (self.registro == self.motor.registro):
+            for asiento in self.asientos:
+                if (type(asiento) == Asiento):
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales" 
+        
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if (tipo == "electrico" or tipo =="gasolina"):
+            self.tipo = tipo
+        
